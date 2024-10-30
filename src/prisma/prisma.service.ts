46e1eed28d74ab86b5450db40468abef9d1a3ff8
unknown <eriicsouzaromeroo@gmail.com>
2024-10-30 15:33:27 -0300

    Add: Prisma Serice

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,23 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService{
-
-  public client: PrismaClient
-
-  constructor(){
-    this.client = new PrismaClient()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      log: ['warn', 'error']
+    })
+  }
+  onModuleInit() {
+    return this.$connect()
+  }
+  onModuleDestroy() {
+    return this.$disconnect()
   }
 
-  teste(){
+  teste() {
     return 'oi'
   }
 }
